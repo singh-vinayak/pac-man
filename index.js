0874403a8d5150e1fff8d25b314b22be260dbe8e
Vinayak Singh <vinayaksingh5302@gmail.com>
2021-02-24 21:45:39 +0530

    added game mechanics

--- index.js
+++ index.js
@@ -123,6 +123,7 @@ function control(e) {
     }
     squares[pacmanCurrentIndex].classList.add('pacman')
     pacDotEaten()
+    powerPelletEaten()
 }
 document.addEventListener('keyup', control)
 
@@ -135,15 +136,39 @@ function pacDotEaten() {
     }
 }
 
+function powerPelletEaten() {
+    //if square pacman is in contains a power pellet
+    if (squares[pacmanCurrentIndex].classList.contains('power-pellet')) {
+        //remove power pellet class
+        squares[pacmanCurrentIndex].classList.remove('power-pellet')
+        //add a score of 10
+        score += 10
+        //change each of the four ghosts to isScared
+        ghosts.forEach(ghost => ghost.isScared = true)
+        //use setTimeout to unscare ghosts after 10 seconds   
+        setTimeout(unScareGhosts, 10000)
+    }
+}
+
+function unScareGhosts() {
+    ghosts.forEach(ghost => ghost.isScared = false)
+}
+
+
+
+
 class Ghost {
     constructor(className, startIndex, speed) {
         this.className = className
         this.startIndex = startIndex
         this.speed = speed
+        this.currentIndex = startIndex
+        this.isScared = false
+        this.timerId = NaN
     }
 }
 
-ghosts = [
+const ghosts = [
     new Ghost('blinky', 348, 250),
     new Ghost('pinky', 376, 400),
     new Ghost('inky', 351, 300),
@@ -151,4 +176,56 @@ ghosts = [
 ]
 
 //draw my ghosts onto my grid
-ghosts.forEach(ghost => squares[ghost.startIndex].classList.add(ghost.className))
+ghosts.forEach(ghost => {
+    squares[ghost.currentIndex].classList.add(ghost.className)
+    squares[ghost.currentIndex].classList.add('ghost')
+})
+
+//move the ghosts
+ghosts.forEach(ghost => moveGhost(ghost))
+
+function moveGhost(ghost) {
+    console.log('moved ghost')
+    const directions = [-1, -width, +1,+width]
+    let direction = directions[Math.floor(Math.random() * directions.length)]
+    console.log(direction)
+
+    ghost.timerId = setInterval(function () {
+        //all our code
+        //if the next square does NOT contain a wall and does not contain a ghost
+        if (
+            !squares[ghost.currentIndex + direction].classList.contains('wall') &&
+            !squares[ghost.currentIndex + direction].classList.contains('ghost')
+        ) {
+            //remove any ghost
+            squares[ghost.currentIndex].classList.remove(ghost.className)
+            squares[ghost.currentIndex].classList.remove('ghost', 'scared-ghost')
+            // //add direction to current Index
+            ghost.currentIndex += direction
+            // //add ghost class
+            squares[ghost.currentIndex].classList.add(ghost.className)
+            squares[ghost.currentIndex].classList.add('ghost')
+        } else direction = directions[Math.floor(Math.random() * directions.length)]
+
+        //if the ghost is currently scared 
+        if (ghost.isScared) {
+            squares[ghost.currentIndex].classList.add('scared-ghost')
+        }
+
+        //if the ghost is current scared AND pacman is on it
+        if (ghost.isScared && squares[ghost.currentIndex].classList.contains('pacman')) {
+            //remove classnames - ghost.className, 'ghost', 'scared-ghost'
+            squares[ghost.currentIndex].classList.remove(ghost.className, 'ghost', 'scared-ghost')
+            // change ghosts currentIndex back to its startIndex
+            ghost.currentIndex = ghost.startIndex
+            //add a score of 100
+            score += 100
+            //re-add classnames of ghost.className and 'ghost' to the ghosts new postion  
+            squares[ghost.currentIndex].classList.add(ghost.className, 'ghost')
+        }
+
+
+
+    }, ghost.speed)
+
+}
